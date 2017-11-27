4523c2b66fdd0f934dc159431c3724bf1d2929fc
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2017-11-27 13:52:22 +0100

    gui further developed

--- MyTunes/src/mytunes/gui/MyTunesController.java
+++ MyTunes/src/mytunes/gui/MyTunesController.java
@@ -7,8 +7,12 @@ package mytunes.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 /**
@@ -20,6 +24,14 @@ public class MyTunesController implements Initializable {
 
     @FXML
     private TextField txtFilter;
+    @FXML
+    private TableView<?> TVPlaylists;
+    @FXML
+    private TableView<?> TVSongs;
+    @FXML
+    private Button btnNewSong;
+    @FXML
+    private Label lblNowPlaying;
 
     /**
      * Initializes the controller class.
@@ -28,5 +40,53 @@ public class MyTunesController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnPlaySong(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnNextSong(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnPreviousSong(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnFilter(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnAddSongToPlaylist(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnNewPlaylist(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnEditPlaylist(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnDeletePlaylist(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnDeleteSongFromPlaylist(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnEditSong(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnDeleteSong(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnClose(ActionEvent event) {
+    }
     
 }
