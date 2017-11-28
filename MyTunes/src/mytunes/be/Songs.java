5e6d1cb7ac15bd2c8275e5ff8e54e0e3c45ed1d0
Bomholt <33052411+Bomholt@users.noreply.github.com>
2017-11-28 12:31:15 +0100

    made song class

--- MyTunes/src/mytunes/be/Songs.java
+++ MyTunes/src/mytunes/be/Songs.java
@@ -11,4 +11,101 @@ package mytunes.be;
  */
 public class Songs {
     
+    private int id;
+    private String name;
+    private String genre;
+    private float duration;
+    private String file;
+
+    /**
+     * Get the value of id
+     *
+     * @return the value of id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param id new value of id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    /**
+     * Get the value of genre
+     *
+     * @return the value of genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * Set the value of genre
+     *
+     * @param genre new value of genre
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    /**
+     * Get the value of duration
+     *
+     * @return the value of duration
+     */
+    public float getDuration() {
+        return duration;
+    }
+
+    /**
+     * Set the value of duration
+     *
+     * @param duration new value of duration
+     */
+    public void setDuration(float duration) {
+        this.duration = duration;
+    }
+    
+    /**
+     * Get the value of file
+     * 
+     * @return the value of file
+     */
+    public String getFile() {
+        return file;
+    }
+
+    /**
+     * Set the value of file
+     *
+     * @param file new value of file
+     */
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+
 }
