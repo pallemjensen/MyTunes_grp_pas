61cbf5252c1c956e2870711529abf3db000e9d25
Anderswith <32036127+Anderswith@users.noreply.github.com>
2017-12-12 13:06:07 +0100

    udkommenterede filter prototype så programmet virker

--- MyTunes/src/mytunes/dal/SongDAO.java
+++ MyTunes/src/mytunes/dal/SongDAO.java
@@ -99,39 +99,39 @@ public class SongDAO {
     }
     
  
-    public void filterSongs(String oldVal, String newVal) {
-       
-    // If the number of characters in the text box is less than last time
-    // it must be because the user pressed delete
-    if ( oldVal != null && (newVal.length() < oldVal.length()) ) {
-        // Restore the lists original set of entries 
-        // and start from the beginning
-        list.setItems( entries );
-    }
-     
-    // Break out all of the parts of the search text 
-    // by splitting on white space
-    String[] parts = newVal.toUpperCase().split(" ");
- 
-    // Filter out the entries that don't contain the entered text
-    ObservableList<String> subentries = FXCollections.observableArrayList();
-    for ( Object entry: list.getItems() ) {
-        boolean match = true;
-        String entryText = (String)entry;
-        for ( String part: parts ) {
-            // The entry needs to contain all portions of the
-            // search string *but* in any order
-            if ( ! entryText.toUpperCase().contains(part) ) {
-                match = false;
-                break;
-            }
-        }
- 
-        if ( match ) {
-            subentries.add(entryText);
-        }
-    }
-    list.setItems(subentries);
+//    public void filterSongs(String oldVal, String newVal) {
+//       
+//    // If the number of characters in the text box is less than last time
+//    // it must be because the user pressed delete
+//    if ( oldVal != null && (newVal.length() < oldVal.length()) ) {
+//        // Restore the lists original set of entries 
+//        // and start from the beginning
+//        list.setItems( entries );
+//    }
+//     
+//    // Break out all of the parts of the search text 
+//    // by splitting on white space
+//    String[] parts = newVal.toUpperCase().split(" ");
+// 
+//    // Filter out the entries that don't contain the entered text
+//    ObservableList<String> subentries = FXCollections.observableArrayList();
+//    for ( Object entry: list.getItems() ) {
+//        boolean match = true;
+//        String entryText = (String)entry;
+//        for ( String part: parts ) {
+//            // The entry needs to contain all portions of the
+//            // search string *but* in any order
+//            if ( ! entryText.toUpperCase().contains(part) ) {
+//                match = false;
+//                break;
+//            }
+//        }
+// 
+//        if ( match ) {
+//            subentries.add(entryText);
+//        }
+//    }
+//    list.setItems(subentries);
 }
     
             
